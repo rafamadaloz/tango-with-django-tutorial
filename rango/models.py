2edaf75008f74d8058224931438c2f22504185c6
Rafael <madalozrafael@gmail.com>
2020-04-03 14:24:14 -0300

    Test Category model

--- rango/models.py
+++ rango/models.py
@@ -10,6 +10,8 @@ class Category(models.Model):
     slug = models.SlugField(blank=True, unique=True)
 
     def save(self, *args, **kwargs):
+        if self.views < 0:
+            self.views = 0
         self.slug = slugify(self.name)
         super(Category, self).save(*args, **kwargs)
 
